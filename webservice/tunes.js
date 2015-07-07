db42fdaa195627405939d49520cb1106736b3ea8
Toby Dacre <toby.junk@gmail.com>
2015-07-07 16:00:55 +0100

    tunes.js major changes a-z etc

--- webservice/tunes.js
+++ webservice/tunes.js
@@ -1,6 +1,6 @@
 /*global $, document, window*/
 
-var TICK_INTERVAL = 500;
+var TICK_INTERVAL = 1500;
 var ERROR_TIMEOUT = 10000;
 
 var feedLock = false;
@@ -9,8 +9,15 @@ var isFullscreen = false;
 var queueVersion = 0;
 var queueItems = {};
 var queue = [];
-var activePage = 'playing';
+var activePage;
 
+var menuDivs = [
+  'controls',
+  'playing',
+  'queue',
+  'artists',
+  'alpha'
+];
 
 function formatTime(time) {
   time = Math.ceil(time);
@@ -175,6 +182,22 @@ function toggleFullscreen() {
   isFullscreen = !isFullscreen;
 }
 
+
+function showPage(page) {
+  var i;
+  for (i = 0; i < menuDivs.length; i++) {
+    if (page !== menuDivs[i]) {
+      $('#' + menuDivs[i]).hide();
+    }
+  }
+  $('#' + page).show();
+  if (page) {
+    $('#menu').hide();
+    activePage = page;
+  }
+}
+
+
 function toggleMenu() {
   var $menu = $('#menu');
   if ($menu.css('display') !== 'none') {
@@ -186,79 +209,172 @@ function toggleMenu() {
   }
 }
 
-var menuDivs = ['playing', 'queue', 'artists', 'tracks'];
 
-function showPage(page) {
-  var i;
-  for (i = 0; i < menuDivs.length; i++) {
-    if (page !== menuDivs[i]) {
-      $('#' + menuDivs[i]).hide();
-    }
-  }
-  $('#' + page).show();
-  if (page) {
-    activePage = page;
-  }
-}
-
-function menuClick(e) {
-  var page = $(e.target).data('page');
+function buttonClick() {
+  var $element = $(this);
+  var page = $element.data('page');
   if (page) {
     showPage(page);
+    // hide menu
+    $('#menu').hide();
+    return;
   }
-  var cmd = $(e.target).data('cmd');
+  var cmd = $element.data('cmd');
   if (cmd) {
-    console.log(cmd);
     $.getJSON('cmd/' + cmd);
-  };
-  // hide menu
-  $('#menu').hide();
+  }
+  if ($element.parent().data('auto') === 'delete'){
+    $element.parent().remove();
+  }
   showPage(activePage);
 }
 
 var info = {};
 
-function buildArtistList(data) {
+function processArtistData(data) {
   var artists = data.artists;
-  artists = artists.concat(artists);
-  artists = artists.concat(artists);
-  artists = artists.concat(artists);
+  artists.sort(function(a, b) {
+    return a[1].toUpperCase() > b[1].toUpperCase();
+  });
+  // artists = artists.concat(artists);
   var artist;
   var lookup = {};
-  var $artist = $('#artists');
   var i;
+  var artistList = [];
   for (i = 0; i < artists.length; i++) {
     artist = artists[i];
     lookup[artist[0]] = artist[1];
-    $artist.append('<div><p data-artist="' + artist[0] + '" ><b>' + artist[1] + '</b></p></div>');
+    artistList.push(artist[0]);
   }
   info.artist = lookup;
-  $('[data-artist]').click(artistInfo);
-  $.getJSON('album.json', buildALbumList);
+  info.artistList = artistList;
+  $.getJSON('album.json', processAlbumData);
+}
+
+function alphaBit(value) {
+  var alpha = value[0].toUpperCase();
+  if (alpha < 'A') {
+    alpha = '#';
+  }
+  return alpha;
 }
 
 
-function tracklistArtist(artist){
-var tracks = info.artistTracks[artist]
-var i;
-var out = [];
-for(i=0;i<tracks.length;i++){
-  out.push('<p>' + info.tracks[tracks[i]][1] + '</p>')
+function artistScroll(name){
+  showPage('artists');
+  var node = document.getElementById(name);
+node.scrollIntoView();
 }
-return out.join('');
+
+
+function buildAlpha(alphas) {
+  var i;
+  var $alpha = $('#alpha').empty();
+  var $list = $('<ul class="button-small">');
+  if (alphas[0] === '#'){
+    alphas.splice(0, 1);
+  }
+
+  for (i=0;i<alphas.length;i++){
+    $list.append('<li><button data-alpha="' + alphas[i] + '">' + alphas[i] + '</button></li>');
+  }
+  $list.on('click', 'button', function (){
+    artistScroll('artist-alpha-' + $(this).data('alpha'));
+  });
+  $alpha.append($list);
 }
-function artistInfo(event) {
-  var $element = $(this);
+
+function buildArtistList() {
+  var alpha;
+  var alphas = [];
+  var alphaLast = '';
+  var artistId;
+  var artistName;
+  var artistTracks;
+  var artistList = info.artistList;
+  var $artist = $('#artists');
+  var listing;
+  var i;
+  for (i = 0; i < artistList.length; i++) {
+    listing = ['<div>'];
+    artistId = artistList[i];
+    artistName = info.artist[artistId];
+    artistTracks = info.artistTracks[artistId].length;
+
+    alpha = alphaBit(artistName);
+    if (alpha !== alphaLast) {
+      listing.push('<a id="artist-alpha-' + alpha + '"></a>');
+      alphaLast = alpha;
+      alphas.push(alpha);
+    }
+
+    listing = listing.concat([
+      '<p data-artist="',
+      artistId,
+      '" ><b>',
+      artistName,
+      '</b> <span>',
+      artistTracks,
+      '</span></p></div>'
+    ]);
+    $artist.append(listing.join(''));
+  }
+  buildAlpha(alphas);
+  $artist.on('click', 'div', artistInfo);
+
+  showPage();
+}
+
+function tracklistArtist(artist) {
+  var track;
+  var tracks = info.artistTracks[artist];
+  var i;
+  var out = [];
+  for (i = 0; i < tracks.length; i++) {
+    track = info.tracks[tracks[i]];
+    out.push('<div><p data-track="'+ track[0] + '">' + track[1] + '</p></div>');
+  }
+  return out.join('');
+}
+
+
+function artistInfo() {
+  var $element = $(this).children('p');
   if ($element.next().length) {
     $element.next().remove();
     return;
   }
   var artist = $element.data('artist');
   $('div.track-list').remove();
-  $(this).after('<div class="track-list">' + tracklistArtist(artist) + '</div>');
+  var listing = [];
+  listing = listing.concat([
+    '<div class="track-list">',
+    tracklistArtist(artist),
+    '</div>'
+  ]);
+  $element.after(listing.join(''));
+  $element.parent()[0].scrollIntoView();
+  $('.track-list').on('click', 'div', trackInfo);
 }
 
-function buildALbumList(data) {
+
+function trackInfo(event){
+  var $element = $(this).children('p');
+  event.stopPropagation();
+  if ($element.next().length) {
+    $element.next().remove();
+    return;
+  }
+  // remove any open controls
+  $element.parent().parent().find('div.track-cmd').remove();
+  var track = $element.data('track');
+  $element.after('<div data-auto="delete" class="track-cmd"><button data-cmd="add-' + track + '">Play</button></div>');
+
+  $element.next().on('click', 'button', buttonClick);
+}
+
+
+function processAlbumData(data) {
   var albums = data.album;
   var album;
   var lookup = {};
@@ -268,10 +384,11 @@ function buildALbumList(data) {
     lookup[album[0]] = album[1];
   }
   info.album = lookup;
-  $.getJSON('track.json', buildTrackList);
+  $.getJSON('track.json', processTrackData);
 }
 
-function buildTrackList(data) {
+
+function processTrackData(data) {
   var track;
   var tracks = data.tracks;
   var i;
@@ -290,10 +407,12 @@ function buildTrackList(data) {
   buildArtistList();
 }
 
+
 $(function() {
   $('#header-title h1').click(toggleFullscreen);
-  $('#menu-button').click(toggleMenu);
-  $('#menu li button').click(menuClick);
-  //  setInterval(tick, TICK_INTERVAL);
+  $('#menu-toggle').click(toggleMenu);
+  $('#page').on('click', 'button', buttonClick);
   resize();
+  setInterval(tick, TICK_INTERVAL);
+  $.getJSON('artist.json', processArtistData);
 });
